3695bbbdbf753f0618781ad655bd00f113905309
Aransiola Kehinde <aransiolaolamiposi@gmail.com>
2026-08-06 04:56:23 +0100

    Homepage restored successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import MainLayout from "../layouts/MainLayout";
+
 import Hero from "../components/Hero";
 import About from "../components/About";
-import Services from "../components/Services";
-import Industries from "../components/Industries";
-import Process from "../components/Process";
 import CTA from "../components/CTA";
 import Stats from "../components/Stats";
 import ServicesPreview from "../components/ServicesPreview";
@@ -12,26 +10,24 @@ import IndustriesGrid from "../components/IndustriesGrid";
 import TrustedBy from "../components/TrustedBy";
 import FeaturedServices from "../components/FeaturedServices";
 import Testimonials from "../components/Testimonials";
+import Process from "../components/Process";
+
 function Home() {
   return (
     <MainLayout>
-      
-        <MainLayout>
-  <Hero />
-  <TrustedBy />
-  <Stats />
-  <ServicesPreview />
-  <About />
-  <FeaturedServices />
-  <WhyChooseUs />
-  <IndustriesGrid />
-  <Process />
-  <Testimonials />
-  <CTA />
-</MainLayout>
-       
+      <Hero />
+      <TrustedBy />
+      <Stats />
+      <ServicesPreview />
+      <About />
+      <FeaturedServices />
+      <WhyChooseUs />
+      <IndustriesGrid />
+      <Process />
+      <Testimonials />
+      <CTA />
     </MainLayout>
-  )
+  );
 }
 
 export default Home;
